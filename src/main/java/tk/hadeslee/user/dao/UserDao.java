4e48531f1ee2e59b9e7f41e44460e04b99311d5f
sklee <hadesleeq@gmail.com>
2017-06-21 16:47:56 +0900

    jdbc를 이용한 등록과 조회 기능이 있는 UserDao 클래스

--- src/main/java/tk/hadeslee/user/dao/UserDao.java
+++ src/main/java/tk/hadeslee/user/dao/UserDao.java
@@ -1,5 +1,9 @@
 package tk.hadeslee.user.dao;
 
+import tk.hadeslee.user.domain.User;
+
+import java.sql.*;
+
 /**
  * Project: springbook
  * FileName: UserDao
@@ -10,4 +14,39 @@ package tk.hadeslee.user.dao;
  * To change this template use File | Settings | File Templates.
  */
 public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "springbook", "springbook");
+        PreparedStatement ps = c.prepareStatement("insert into users (id,name,password) VALUE (?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "springbook", "springbook");
+        PreparedStatement ps = c.prepareStatement("SELECT * FROM users WHERE id=?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
 }
